b3bdfff2439f99aca16f95c51de4b1d3e40962d3
Sam Marrello <sammarrello@gmail.com>
2022-01-07 20:24:09 -0500

    setting up basic html and CSS

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,13 @@
 package com.example.plugins
 
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.http.content.*
 
 fun Application.configureRouting() {
-
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        static("/static") {
+            resources("html")
         }
     }
 }
